a4173e97695b3ece984fd0663f24becbd177755b
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-06-10 10:20:05 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Василий Полторак on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
